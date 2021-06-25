bd16b016507a99b2ec80ed9f03becca5a6467e35
ppidcard <tvxqlikei@live.cn>
2021-06-25 15:59:30 +1000

    Done

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
 #write your code here
 
-def countdown
+def countdown(number)
+    while number >= 1
+        puts "#{number} SECOND(S)!"
+        number -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+countdown(10)
+
+def countdown_with_sleep(number)
+    
+    while number >= 1
+        puts "#{number} SECOND(S)!"
+        number -= 1
+        sleep(1)
+    end
+    "HAPPY NEW YEAR!"
+
+
 end
